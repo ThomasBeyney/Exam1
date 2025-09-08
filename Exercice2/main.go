673d1c3551fb687d56daf331e491d87a9efe3827
ThomasBeyney <thomas.beyney@ynov.com>
2025-09-08 12:15:16 +0200

    Exercice3

---
+++ Exercice2/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(str string){
+	j := 0
+	for i := 0 ; i <= len(str); i++ {
+		if str[i] (str[i] <= 'z' && str[i] >= 'a') || (str[i] <= 'Z' && str[i] >= 'A'){
+			j++
+		}
+		fmt.Print(j)
+	}
+}
