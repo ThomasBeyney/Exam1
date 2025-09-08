dd35ae69649562b87f2948966a11970460691e59
ThomasBeyney <thomas.beyney@ynov.com>
2025-09-08 12:30:01 +0200

    Exercice2

--- Exercice2/main.go
+++ Exercice2/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main(str string){
+func Lettre(str string){
 	j := 0
 	for i := 0 ; i <= len(str); i++ {
 		if str[i] (str[i] <= 'z' && str[i] >= 'a') || (str[i] <= 'Z' && str[i] >= 'A'){
@@ -10,6 +10,10 @@ func main(str string){
 		}
 		fmt.Print(j)
 	}
+}
+
+func main(){
+	fmt.Println(Lettre("Hello,World!"))
 }
 
 //t'embète pas sa marche pas :/
