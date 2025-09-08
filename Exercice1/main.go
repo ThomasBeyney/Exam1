c3fcfc9eb78f0dd26e44eb3dce54e283fabe9fb5
ThomasBeyney <thomas.beyney@ynov.com>
2025-09-08 11:04:39 +0200

    Exercice1

---
+++ Exercice1/main.go
@@ -0,0 +1,12 @@
+package main
+
+func Surprise(s string) bool {
+	if len(s) < 1 {         
+		return false 
+    }     
+	return surprise_annexe(s[0]) || Surprise(s[1:]) 
+}  
+
+func surprise_annexe(a byte) bool {     
+	return (a <= 'z' && a >= 'a') || (a <= 'Z' && a >= 'A') 
+}
